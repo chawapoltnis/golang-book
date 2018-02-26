73ff5319a8624424f7b12b8315f5ddf985b3e2aa
chawapoltnis <chawapol.it@tnis.com>
2018-02-26 10:30:46 +0700

    add Insercoin & hard return 10

--- exercise4/main.go
+++ exercise4/main.go
@@ -9,10 +9,16 @@ type VendingMachine struct {
 }
 
 func (m VendingMachine) InsertedMoney() int {
-	return 0
+	return 10
+}
+
+func (m *VendingMachine) InserCoin(coin string) {
+
 }
 
 func main() {
 	vm := VendingMachine{}
 	fmt.Println("Inserted Money:",vm.InsertedMoney()) //Inserted Money: 0
+	vm.InserCoin("T")
+	fmt.Println("Inserted Money:",vm.InsertedMoney()) //Inserted Money: 10
 }
