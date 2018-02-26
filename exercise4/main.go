f161b22c9fa014bbbb4f90aa96e3697be3b8bedb
chawapoltnis <chawapol.it@tnis.com>
2018-02-26 10:37:38 +0700

    add condition in function

--- exercise4/main.go
+++ exercise4/main.go
@@ -13,12 +13,26 @@ func (m VendingMachine) InsertedMoney() int {
 }
 
 func (m *VendingMachine) InserCoin(coin string) {
-	m.insertedMoney=10
+	if coin=="T" {
+		m.insertedMoney+=10
+	}
+	if coin=="F" {
+		m.insertedMoney+=5
+	}
+	if coin=="TW" {
+		m.insertedMoney+=2
+	}
+	if coin=="O" {
+		m.insertedMoney+=1
+	}
 }
 
 func main() {
 	vm := VendingMachine{}
 	fmt.Println("Inserted Money:",vm.InsertedMoney()) //Inserted Money: 0
 	vm.InserCoin("T")
+	vm.InserCoin("F")
+	vm.InserCoin("TW")
+	vm.InserCoin("O")
 	fmt.Println("Inserted Money:",vm.InsertedMoney()) //Inserted Money: 10
 }
