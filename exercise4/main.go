76518c5b282bf25b9f9ef617bc5891197df0e1e3
chawapoltnis <chawapol.it@tnis.com>
2018-02-26 10:49:15 +0700

    add func buySD and hardcode return

--- exercise4/main.go
+++ exercise4/main.go
@@ -17,6 +17,10 @@ func (m *VendingMachine) InserCoin(coin string) {
 	m.insertedMoney += m.coins[coin]
 }
 
+func (m *VendingMachine) SelectSD() string {
+	return "SD"
+}
+
 func main() {
 	var coins = map[string]int{"T":10,"F":5,"TW":2,"O":1}
 	vm := VendingMachine{coins:coins}
@@ -26,4 +30,6 @@ func main() {
 	vm.InserCoin("TW")
 	vm.InserCoin("O")
 	fmt.Println("Inserted Money:",vm.InsertedMoney()) //Inserted Money: 10
+	can := vm.SelectSD()
+	fmt.Println(can)
 }
