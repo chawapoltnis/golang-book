a4eff7e24f2261423026309e60ad07aa7da6a1ec
chawapoltnis <chawapol.it@tnis.com>
2018-02-26 10:26:23 +0700

    initial struct and insertedmoney 0

--- exercise4/main.go
+++ exercise4/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+)
+
+type VendingMachine struct {
+
+}
+
+func (m VendingMachine) InsertedMoney() int {
+	return 0
+}
+
 func main() {
-	
+	vm := VendingMachine{}
+	fmt.Println("Inserted Money:",vm.InsertedMoney()) //Inserted Money: 0
 }
