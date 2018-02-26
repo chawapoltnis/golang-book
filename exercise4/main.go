555b508a4d3e9b2923f921babde2cf8d1c7c18c3
chawapoltnis <chawapol.it@tnis.com>
2018-02-26 10:45:13 +0700

    add map in struct

--- exercise4/main.go
+++ exercise4/main.go
@@ -6,6 +6,7 @@ import (
 
 type VendingMachine struct {
 	insertedMoney int
+	coins map[string]int
 }
 
 func (m VendingMachine) InsertedMoney() int {
@@ -13,22 +14,12 @@ func (m VendingMachine) InsertedMoney() int {
 }
 
 func (m *VendingMachine) InserCoin(coin string) {
-	if coin=="T" {
-		m.insertedMoney+=10
-	}
-	if coin=="F" {
-		m.insertedMoney+=5
-	}
-	if coin=="TW" {
-		m.insertedMoney+=2
-	}
-	if coin=="O" {
-		m.insertedMoney+=1
-	}
+	m.insertedMoney += m.coins[coin]
 }
 
 func main() {
-	vm := VendingMachine{}
+	var coins = map[string]int{"T":10,"F":5,"TW":2,"O":1}
+	vm := VendingMachine{coins:coins}
 	fmt.Println("Inserted Money:",vm.InsertedMoney()) //Inserted Money: 0
 	vm.InserCoin("T")
 	vm.InserCoin("F")
